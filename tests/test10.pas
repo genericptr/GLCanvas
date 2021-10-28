ed10a969b8d3b77f87500e74accb19b4fb747c89
Ryan Joseph <genericptr@gmail.com>
2021-10-28 08:59:04 +0700

    changes to test 10

--- tests/test10.pas
+++ tests/test10.pas
@@ -16,7 +16,7 @@ uses
 const
   window_size_width = 512;
   window_size_height = 512;
-  cell_size = 100;
+  cell_size = 512 / 4;
 
 var
   textures: array[0..3] of TTexture;
@@ -30,10 +30,7 @@ begin
   textures[2] := TTexture.Create('human.png');
   textures[3] := TTexture.Create('centaur.png');
 
-  { IMPORTANT: If we don't make the frame buffer the same size as the window then
-    we need to push a new projection matrix before drawing. }
-
-  frameBuffer := TFrameBuffer.Create(GetWindowSize);
+  frameBuffer := TFrameBuffer.Create(256);
   frameBuffer.Push;
     FillRect(frameBuffer.Bounds, TColor.SandyBrown);
     for y := 0 to 1 do
@@ -41,10 +38,13 @@ begin
       DrawTexture(textures[x + y * 2], RectMake(x * cell_size, y * cell_size, cell_size, cell_size));
   frameBuffer.Pop;
 
+
   while IsRunning do
     begin
       FillRect(GetViewPort, TColor.Red);
-      DrawTexture(frameBuffer.Texture, frameBuffer.Bounds);
+      for y := 0 to 1 do
+      for x := 0 to 1 do
+        DrawTexture(frameBuffer.Texture, RectMake(x * cell_size * 2, y * cell_size * 2, frameBuffer.Width, frameBuffer.Height));
       SwapBuffers;
     end;
 
