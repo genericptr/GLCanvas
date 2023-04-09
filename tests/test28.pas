6b173216843295ca43b59789f1b6c39dbb1a40b9
Ryan Joseph <ryan@mimestream.com>
2023-04-09 10:17:11 +0700

    frame buffer naming

--- tests/test28.pas
+++ tests/test28.pas
@@ -8,25 +8,38 @@
 {$mode objfpc}
 {$modeswitch multihelpers}
 
-program Test10;
+program Test28;
 uses
-  GLCanvas;
+  SysUtils, GLCanvas;
 
 const
   window_size_width = 512;
   window_size_height = 512;
-  cell_size = 512 / 4;
 
 var
   frameBuffer: TFrameBuffer;
+  i: integer;
 begin
   SetupCanvas(window_size_width, window_size_height);
 
-  // Set up frame buffer
+  // create frame buffers and delete them
+  for i := 1 to 100 do
+    begin
+      frameBuffer := TFrameBuffer.Create(256);
+      frameBuffer.Push;
+        FillRect(frameBuffer.Bounds, TColor.Black);
+      frameBuffer.Pop;
+      frameBuffer.Free;
+    end;
+
+  // create the real frame buffer and ensure the IDs are correct (should be 1)
   frameBuffer := TFrameBuffer.Create(256);
   frameBuffer.Push;
     FillRect(frameBuffer.Bounds, TColor.SandyBrown);
   frameBuffer.Pop;
+
+  Assert(frameBuffer.BufferID = 1, 'Frame buffer ID should be 1 (got '+frameBuffer.BufferID.ToString+')');
+  Assert(frameBuffer.TextureID = 1, 'Frame buffer texture should be 1 (got '+frameBuffer.TextureID.ToString+')');
 
   while IsRunning do
     begin
